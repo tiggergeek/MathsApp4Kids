d0796ddbe421666d802c7f3ef090163dcfa51698
tiggergeek <michael_mccready@hotmail.com>
2017-04-28 15:41:54 +0100

    Initial upload of eclipse files

---
+++ test/unitTests/TestPlayer.java
@@ -0,0 +1,44 @@
+package unitTests;
+
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+public class TestPlayer {
+
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@After
+	public void tearDown() throws Exception {
+	}
+
+	@Test
+	public void testGetName() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testSetName() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetHighScore() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testSetHighScore() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testPlayer() {
+		fail("Not yet implemented");
+	}
+
+}
